e363c572737dc6321d3b8c86702a975062bda3ad
AndreyKhmara <kelevra101smu@gmail.com>
2025-12-02 13:20:18 +0300

    add get all todos

--- todo/views.py
+++ todo/views.py
@@ -3,11 +3,10 @@ from todo.models import Todo
 from todo.serializers import TodoSerializer
 
 class TodoListCreateView(generics.ListCreateAPIView):
-    queryset = Todo.objects.all()
+    queryset = Todo.objects.all() # в теории вот это для GET (получение всех todo)
     serializer_class = TodoSerializer
 
-    def create(self, request, *args, **kwargs):
-        print("🔥 POPAL V CREATE!")  # 1 — Проверяем вход
-        print("📦 DATA:", request.data)  # 2 — payload
-
+    def create(self, request, *args, **kwargs): #  def create для POST (Создание todo)
+        print("🔥 POPAL V CREATE!")
+        print("📦 DATA:", request.data)
         return super().create(request, *args, **kwargs)
